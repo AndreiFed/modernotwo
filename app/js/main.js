851f8a52ac789d49efcfdbc7089102e5c8916df0
AndreiFed <andreif5@yandex.ru>
2020-05-24 08:17:23 +0300

    new-items

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,24 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500
+
+    });
+    $(".icon-th-list").on('click', function() {
+        $('.product__item').addClass('list');
+        $(".icon-th-list").addClass('active');
+        $(".icon-th-large").removeClass('active');
+    });
+    $(".icon-th-large").on('click', function() {
+        $('.product__item').removeClass('list');
+        $(".icon-th-list").removeClass('active');
+        $(".icon-th-large").addClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
 
